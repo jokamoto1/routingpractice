9bcbe0f2a983eef5a51e68c89a6e9237a35d9045
Jeremy Okamoto <okamotojeremy@gmail.com>
2021-09-07 12:14:46 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
+
+const Home = (props) => {
+  return (
+    <h1>Welcome</h1>
+  );
+}
+const Param = (props) => {
+  const { param } = useParams()
+  if (isNaN(param)) {
+    return (
+      <h1>The word is: {param}</h1>
+
+    );
+  }
+  if (!isNaN(param)) {
+    return (
+      <h1>The number is: {param}</h1>
+    );
+  }
+}
+const Param2 = (props) => {
+  const { param } = useParams()
+  const { text } = useParams()
+  const { bg } = useParams()
+  if (isNaN(param)) {
+    return (
+      <h1 style={{ color: text, background:  bg  }}>The word is: {param}</h1>
+      );
+    }
+    if (!isNaN(param)) {
+      return (
+      <h1 style={{ color: text, background:  bg  }}>The number is: {param}</h1>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/:param">
+          <Param />
+        </Route>
+        <Route exact path="/:param/:text/:bg">
+          <Param2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
